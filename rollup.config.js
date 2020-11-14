4a0e706d66cd6ad2d910cc454b53150067f722b8
monochromer <monochromer@mail.ru>
2020-11-15 01:30:19 +0500

    build: update dev deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
 import changeCase from 'change-case';
 import pkg from './package.json';
 
